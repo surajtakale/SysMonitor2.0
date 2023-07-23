06a7c17d5a4a0247388f02012604690f19d7ad30
Suraj <suraj.takale@elastic.run>
2023-07-23 14:01:11 +0530

    fixes

--- src/format.cpp
+++ src/format.cpp
@@ -1,6 +1,7 @@
 #include <string>
 
 #include "format.h"
+#include "iostream"
 
 using std::string;
 
@@ -9,16 +10,19 @@ using std::string;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) { 
-    // return string(); 
+    // return string();
+    std::cout<<"Inside Format "<<std::endl;
     std::string result;
-    
+    std::cout<<"Inside Format 2"<<std::endl;
     std::string h = std::to_string(seconds / 3600);
     std::string m = std::to_string((seconds % 3600) / 60);
     std::string s = std::to_string(seconds % 60);
+    std::cout<<"Inside Format 3"<<std::endl;
     // add leading zero if needed
     std::string hh = std::string(2 - h.length(), '0') + h;
     std::string mm = std::string(2 - m.length(), '0') + m;
     std::string ss = std::string(2 - s.length(), '0') + s;
+    std::cout<<"Inside Format 4"<<std::endl;
     // return mm:ss if hh is 00
     if (hh.compare("00") != 0) {
         result = hh + ':' + mm + ":" + ss;
