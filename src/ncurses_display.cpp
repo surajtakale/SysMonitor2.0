a646b25a2ca10f81258f4319b7b07a79e6d42cf1
Suraj <suraj.takale@elastic.run>
2023-07-23 13:35:01 +0530

    fixes

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -97,7 +97,7 @@ void NCursesDisplay::Display(System& system, int n) {
   WINDOW* process_window =
       newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
 
-  while (1) {
+  // while (1) {
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     box(system_window, 0, 0);
@@ -108,6 +108,6 @@ void NCursesDisplay::Display(System& system, int n) {
     wrefresh(process_window);
     refresh();
     std::this_thread::sleep_for(std::chrono::seconds(1));
-  }
+  // }
   endwin();
 }
