4633458482732920ae099c65f850d5f4c54267ed
Suraj <suraj.takale@elastic.run>
2023-07-23 10:17:48 +0530

    fixes

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,12 +1,15 @@
 #include "processor.h"
 #include "linux_parser.h"
+#include "iostream"
 
 
 
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
       long totalJiffies = LinuxParser::Jiffies();
+      std::cout<<"totalJiffies:- "<<totalJiffies<<std::endl;
   long activeJiffies = LinuxParser::ActiveJiffies();
+  std::cout<<"activeJiffies:- "<<activeJiffies<<std::endl;
   float utilization = static_cast<float>(activeJiffies) / totalJiffies;
   return utilization;
  }
