1ef22506735ab843527f5bc1c235876425af121a
Suraj <suraj.takale@elastic.run>
2023-07-23 09:39:16 +0530

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "ncurses_display.h"
 #include "system.h"
 
+#include "iostream"
+
 int main() {
   System system;
-  NCursesDisplay::Display(system);
+  std::cout<<"Welcome to the Curses Display";
+  std::cout<<system.Kernel();
+  // NCursesDisplay::Display(system);
 }
