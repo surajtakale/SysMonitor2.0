846873abd885300f75fc9465d304baa77df43869
Suraj <suraj.takale@elastic.run>
2023-07-23 09:59:45 +0530

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,8 @@ int main() {
   System system;
   // std::cout<<"Welcome to the Curses Display";
   // std::cout<<system.Kernel();
-  NCursesDisplay::Display(system);
+  // NCursesDisplay::Display(system);
+  for (int i=0; i < system.Processes().size(); i++){
+    std::cout<<system.Processes()[i].Command();
+  }
 }
